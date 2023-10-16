f9a1ce8c25efac0fc2cae98cd90e33f047cde7ee
rohitwtbs <12rohit4@gmail.com>
2023-10-16 22:35:07 +0530

    adding the fucking wrong code just to increase commit haahhaa

--- luajobs/django-docker-template/website/jobs/views.py
+++ luajobs/django-docker-template/website/jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
 
 # Create your views here.
+
+
+def views():
+    return HttpResponse('this is the first job opening')
